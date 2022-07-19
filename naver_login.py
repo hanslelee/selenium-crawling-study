cfdba35626c9001788855f1247dc6adcd9027819
hanslelee <hssarah13@gmail.com>
2022-07-19 23:11:05 +0900

    naver login

--- naver_login.py
+++ naver_login.py
@@ -7,5 +7,9 @@ driver = webdriver.Chrome("./chromedriver")
 # 크롬웹드라이버가 네이버 로그인창띄움
 driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com")
 
-# 아이디 요소 가져와서 인풋에 넣어봄
+# 아이디, 비번 요소 가져와서 인풋에 넣어봄
 driver.find_element(By.ID, "id").send_keys("룰루")
+driver.find_element(By.ID, "pw").send_keys("qwer1234")
+
+# 로그인 버튼 클릭!
+driver.find_element(By.ID, "log.login").click()
